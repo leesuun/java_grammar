c552a94a2efbbfc82cd02aefbbe3e05711c6c4ab
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2023-01-18 11:17:56 +0900

    testcommit

---
+++ src/java_grammer/testclass.java
@@ -0,0 +1,11 @@
+package java_grammer;
+
+public class testclass {
+
+	public testclass() {
+		// TODO Auto-generated constructor stub
+		System.out.println("s");
+		System.out.println("s");
+	}
+
+}
