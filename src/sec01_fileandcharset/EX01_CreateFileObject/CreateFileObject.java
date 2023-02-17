989910104403cdef402b7b142a3ed6eabb0b9505
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2023-02-17 11:08:43 +0900

    [JAVA] 파일 입출력(offset, length, count2)

--- src/sec01_fileandcharset/EX01_CreateFileObject/CreateFileObject.java
+++ src/sec01_fileandcharset/EX01_CreateFileObject/CreateFileObject.java
@@ -3,6 +3,7 @@ package sec01_fileandcharset.EX01_CreateFileObject;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class CreateFileObject {
@@ -11,18 +12,34 @@ public class CreateFileObject {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
-		System.out.printf("찾을 파일의 경로와 파일 이름 입력: ");
+		System.out.printf("읽을 파일 경로: ");
 		String FilePathAndName = sc.next();
 
 		try {
 			InputStream is = new FileInputStream(FilePathAndName);
-			int data;
 
-			while ((data = is.read()) != -1) {
+			byte[] byteArray1 = new byte[9];
+			int count;
 
-				System.out.println("읽은 데이터: " + (char) data + " 남은 바이트 수: " + is.available());
+			while ((count = is.read(byteArray1)) != -1) {
+				String str = new String(byteArray1, 0, count, Charset.forName("UTF-8"));
+				System.out.println(str);
+				System.out.println(": count = " + count);
+
 			}
 			is.close();
+			System.out.println();
+			System.out.println();
+
+			InputStream is2 = new FileInputStream(FilePathAndName);
+			byte[] byteArray2 = new byte[9];
+			int offset = 3;
+			int length = 6;
+			int count2 = is2.read(byteArray2, offset, length);
+			System.out.println(new String(byteArray2, 0, offset + count2, Charset.forName("UTF-8")));
+			System.out.println(new String(byteArray2, 0, offset + count2, Charset.forName("MS949")));
+			is2.close();
+
 		} catch (IOException e) {
 			// TODO: handle exception
 			System.out.println("IOException:" + e);
